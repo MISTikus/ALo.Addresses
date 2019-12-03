786af4f28ca0962c1057e9c23ab919a2ccc79d0b
MTimoshinin <lord.mistikus@gmail.com>
2019-12-03 21:31:53 +0200

    init project structure

---
+++ src/ALo.Addresses.FiasUpdater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ALo.Addresses.FiasUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
