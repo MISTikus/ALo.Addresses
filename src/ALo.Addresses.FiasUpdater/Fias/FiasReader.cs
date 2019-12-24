12fc1a90624f8a5a0cd909f3c697c856dcf50d6d
MTimoshinin <lord.mistikus@gmail.com>
2019-12-24 22:42:56 +0200

    feat: add progress event

--- src/ALo.Addresses.FiasUpdater/Fias/FiasReader.cs
+++ src/ALo.Addresses.FiasUpdater/Fias/FiasReader.cs
@@ -7,13 +7,31 @@ namespace ALo.Addresses.FiasUpdater.Fias
 {
     public class FiasReader
     {
+        public delegate void ProgressChangedEventHander(object sender, double progressPersentage);
+        public event ProgressChangedEventHander ProgressChanged;
+
         public async IAsyncEnumerable<T> Read<T>(string fileName, string rootNode, XmlSerializer serializer) where T : class
         {
-            using var reader = XmlReader.Create(new StreamReader(fileName), new XmlReaderSettings { Async = true, IgnoreWhitespace = true });
+            using var sreamReader = new StreamReader(fileName);
+            using var reader = XmlReader.Create(sreamReader, new XmlReaderSettings { Async = true, IgnoreWhitespace = true });
             reader.ReadToFollowing(rootNode);
+            long lastPosition = 0;
+            var previous = "";
             while (await reader.ReadAsync() && reader.NodeType != XmlNodeType.EndElement)
             {
                 var data = serializer.Deserialize(reader.ReadSubtree()) as T;
+
+                if (lastPosition != sreamReader.BaseStream.Position)
+                {
+                    lastPosition = sreamReader.BaseStream.Position;
+                    var progress = 100.0 * lastPosition / sreamReader.BaseStream.Length;
+                    if (progress.ToString("F2") != previous)
+                    {
+                        previous = progress.ToString("F2");
+                        ProgressChanged.Invoke(this, progress);
+                    }
+                }
+
                 yield return data;
             }
         }
--- src/ALo.Addresses.FiasUpdater/Fias/FiasUpdater.cs
+++ src/ALo.Addresses.FiasUpdater/Fias/FiasUpdater.cs
@@ -1,6 +1,7 @@
 ﻿using ALo.Addresses.FiasUpdater.Configuration;
 using Microsoft.Extensions.Options;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
@@ -10,43 +11,43 @@ namespace ALo.Addresses.FiasUpdater.Fias
     {
         private readonly IOptions<Source> sourceOptions;
         private readonly FiasReader reader;
+        private readonly Stopwatch watcher;
         private readonly Func<Type, XmlSerializer> serializerFactory;
 
         public FiasUpdater(IOptions<Source> sourceOptions, FiasReader reader)
         {
             this.sourceOptions = sourceOptions;
             this.reader = reader;
+            this.watcher = new Stopwatch();
+            this.reader.ProgressChanged += (s, a) => Console.WriteLine($"Progress: {a.ToString("F2")}%. " +
+                $"Total: {TimeSpan.FromSeconds(100 * ((int)this.watcher.Elapsed.TotalSeconds) / a).ToString(@"hh\:mm\:ss")} " +
+                $"Left: {(TimeSpan.FromSeconds(100 * ((int)this.watcher.Elapsed.TotalSeconds) / a) - this.watcher.Elapsed).ToString(@"hh\:mm\:ss")}");
             this.serializerFactory = t => new XmlSerializer(t);
         }
 
         internal async Task Update()
         {
-            var sw = new System.Diagnostics.Stopwatch();
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             var fileName = @"D:\Downloads\Abyss\Fias\some.xml";
-            //var fileName = @"D:\Downloads\Abyss\Fias\Xml\AS_ADDROBJ_20191201_203452ff-36b0-4d2f-956d-f73ec29b2440.XML";
-            var i = 1;
-            sw.Start();
+            this.watcher.Start();
             //await foreach (var data in this.reader.Read<Models.AddressObject>(fileName, "AddressObjects", this.serializerFactory(typeof(Models.AddressObject))))
             //{
             //    Console.WriteLine($"{i} : {data.Level}: {data.ShortTypeName} {data.OfficialName}");
             //    i++;
             //}
-            Console.WriteLine($"Elapsed: {sw.Elapsed}");
+            Console.WriteLine($"Elapsed: {this.watcher.Elapsed.ToString(@"hh\:mm\:ss")}");
 
             //fileName = @"D:\Downloads\Abyss\Fias\some_h.xml";
             fileName = @"D:\Downloads\Abyss\Fias\Xml\AS_HOUSE_20191201_f9a27344-f645-452c-8019-ae1354554774.XML";
-            i = 1;
-            sw.Restart();
+            this.watcher.Restart();
 
             await foreach (var data in this.reader.Read<Models.HouseObject>(fileName, "Houses", this.serializerFactory(typeof(Models.HouseObject))))
             {
-                Console.WriteLine($"{i} : {data.AddressId}: {data.HouseNumber} {data.BuildingNumber} {data.StructureNumber}");
-                i++;
+                //i++;
             }
 
-            Console.WriteLine($"Elapsed: {sw.Elapsed}");
+            Console.WriteLine($"Elapsed: {this.watcher.Elapsed.ToString(@"hh\:mm\:ss")}");
         }
     }
 }
